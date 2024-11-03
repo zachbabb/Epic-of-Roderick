f33fd824a589a9b300b4f038910b538bcf036afd
zach-planet-nine <zach@planetnine.app>
2024-11-02 23:22:58 -0700

    emitters rise again!

--- utils/pex2json/map-pex-2-savage.js
+++ utils/pex2json/map-pex-2-savage.js
@@ -1,47 +1,54 @@
+var { gunzipSync } = require('zlib');
+
 module.exports = (config) => {
 console.log(config);
   const mapped = {};
 
+  mapped.image = config.texture.name;
+console.log(typeof config.texture.data);
+  if(config.texture.data) {
+    const imageBuffer = gunzipSync(Buffer.from(config.texture.data, 'base64'));
+    mapped.imageData = 'data:image/png;base64,' + imageBuffer.toString('base64');
+    console.log(mapped.imageData);
+  }
+
   mapped.minX = +config.sourcePosition.x - +config.sourcePositionVariance.x;
   mapped.maxX = +config.sourcePosition.x + +config.sourcePositionVariance.x;
-  mapped.minY = +config.sourcePosition.Y - +config.sourcePositionVariance.y;
-  mapped.maxY = +config.sourcePosition.Y + +config.sourcePositionVariance.y;
+  mapped.minY = +config.sourcePosition.y - +config.sourcePositionVariance.y;
+  mapped.maxY = +config.sourcePosition.y + +config.sourcePositionVariance.y;
 
   const degreesToRadians = (degrees) => degrees * Math.PI / 180;
-  mapped.minVX = +config.speed.value * Math.cos(degreesToRadians(+config.angle.value - +config.angleVariance.value));
-  mapped.maxVX = +config.speed.value * Math.cos(degreesToRadians(+config.angle.value + +config.angleVariance.value));
-  mapped.minVY = +config.speed.value * Math.sin(degreesToRadians(+config.angle.value - +config.angleVariance.value));
-  mapped.maxVY = +config.speed.value * Math.sin(degreesToRadians(+config.angle.value + +config.angleVariance.value));
+  const roundToZero = (value) => value < 0.01 ? 0 : value;
+  mapped.minVX = +config.speed.value * roundToZero(Math.cos(degreesToRadians(+config.angle.value - +config.angleVariance.value)));
+  mapped.maxVX = +config.speed.value * roundToZero(Math.cos(degreesToRadians(+config.angle.value + +config.angleVariance.value)));
+  mapped.minVY = +config.speed.value * roundToZero(Math.sin(degreesToRadians(+config.angle.value - +config.angleVariance.value)));
+  mapped.maxVY = +config.speed.value * roundToZero(Math.sin(degreesToRadians(+config.angle.value + +config.angleVariance.value)));
 
   mapped.minLifetime = +config.particleLifeSpan.value - +config.particleLifespanVariance.value;
   mapped.maxLifetime = +config.particleLifeSpan.value + +config.particleLifespanVariance.value;
 
-  mapped.minAX = config.gravity.x;
-  mapped.maxAX = config.gravity.x;
-  mapped.minAY = config.gravity.y;
-  mapped.maxAY = config.gravity.y;
+  mapped.minAX = +config.gravity.x;
+  mapped.maxAX = +config.gravity.x;
+  mapped.minAY = +config.gravity.y;
+  mapped.maxAY = +config.gravity.y;
 
   if(Math.abs(+config.radialAcceleration.value) > 0.01 || Math.abs(+config.tangentialAcceleration.value) > 0.01) {
 console.log('this one.');
   } 
 
-  mapped.startColor = {
-    minR: Math.floor(255 / (+config.startColor.red - +config.startColorVariance.red)),
-    maxR: Math.floor(255 / (+config.startColor.red + +config.startColorVariance.red)),
-    minG: Math.floor(255 / (+config.startColor.green - +config.startColorVariance.green)),
-    maxG: Math.floor(255 / (+config.startColor.green + +config.startColorVariance.green)),
-    minB: Math.floor(255 / (+config.startColor.blue - +config.startColorVariance.blue)),
-    maxB: Math.floor(255 / (+config.startColor.blue + +config.startColorVariance.blue))
-  };
+  mapped.minR = Math.floor(255 * (+config.startColor.red - +config.startColorVariance.red));
+  mapped.maxR = Math.floor(255 * (+config.startColor.red + +config.startColorVariance.red));
+  mapped.minG = Math.floor(255 * (+config.startColor.green - +config.startColorVariance.green));
+  mapped.maxG = Math.floor(255 * (+config.startColor.green + +config.startColorVariance.green));
+  mapped.minB = Math.floor(255 * (+config.startColor.blue - +config.startColorVariance.blue));
+  mapped.maxB = Math.floor(255 * (+config.startColor.blue + +config.startColorVariance.blue));
 
-  mapped.endColor = {
-    minR: Math.floor(255 / (+config.finishColor.red - +config.finishColorVariance.red)),
-    maxR: Math.floor(255 / (+config.finishColor.red + +config.finishColorVariance.red)),
-    minG: Math.floor(255 / (+config.finishColor.green - +config.finishColorVariance.green)),
-    maxG: Math.floor(255 / (+config.finishColor.green + +config.finishColorVariance.green)),
-    minB: Math.floor(255 / (+config.finishColor.blue - +config.finishColorVariance.blue)),
-    maxB: Math.floor(255 / (+config.finishColor.blue + +config.finishColorVariance.blue))
-  };
+  mapped.minR = Math.floor(255 * (+config.finishColor.red - +config.finishColorVariance.red));
+  mapped.maxR = Math.floor(255 * (+config.finishColor.red + +config.finishColorVariance.red));
+  mapped.minG = Math.floor(255 * (+config.finishColor.green - +config.finishColorVariance.green));
+  mapped.maxG = Math.floor(255 * (+config.finishColor.green + +config.finishColorVariance.green));
+  mapped.minB = Math.floor(255 * (+config.finishColor.blue - +config.finishColorVariance.blue));
+  mapped.maxB = Math.floor(255 * (+config.finishColor.blue + +config.finishColorVariance.blue));
 
   mapped.maxParticles = config.maxParticles.value;
 
--- utils/pex2json/pex2json.js
+++ utils/pex2json/pex2json.js
@@ -4,13 +4,13 @@ var parser = require('xml2json');
 var p2s = require('./map-pex-2-savage.js');
  
 
-const files = fs.readdirSync('../../Epic-of-Roderick/').filter(file => path.extname(file).toLowerCase() === '.pex');
+const files = fs.readdirSync('../../').filter(file => path.extname(file).toLowerCase() === '.pex');
 console.log(files.length);
 
 let count = 0;
 
 const mapFileToJSON = (file) => { 
-  const xml = fs.readFileSync('../../Epic-of-Roderick/' + files[0]);
+  const xml = fs.readFileSync('../../' + file);
   var json = parser.toJson(xml);
   console.log("to json -> %s", json);
 
@@ -19,9 +19,20 @@ const mapFileToJSON = (file) => {
   console.log('and then mapped is:');
   var mapped = p2s(obj.particleEmitterConfig);
   console.log(mapped);
+  fs.writeFileSync(`./json-emitters/${file.split('.')[0]}`, JSON.stringify(mapped));
   count++;
+  return mapped;
 };
 
-files.forEach(file => mapFileToJSON(file));
+let images = [];
+files.forEach(file => {
+  const json = mapFileToJSON(file);
+  if(!json) return;
+  images.push(json.image);
+});
+let foo = {};
+console.log(images);
+images.forEach(image => foo[image] = true);
+console.log(foo);
 
 console.log('converted ' + count + ' of ' + files.length + ' emitters to SaVaGe');
